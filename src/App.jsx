426e1f0a1c996f8c2d3fa73921d7e98a920308f7
MattMan007 <mateyonescu5@gmail.com>
2024-03-17 19:03:24 +0200

    Hero Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
-import styles from "./style"
+import styles from './style';
+import {Billing,Business,Button,CardDeal,Client,CTA,FeedbackCard,Footer,GetStarted,Hero,Navbar,Stats,Testimonials} from './components';
 
 const App = () => {
   return (
     <div className='bg-primary w-full overflow-hidden'>
       <div className={`${styles.paddingX} ${styles.flexCenter}` }>
         <div className={`${styles.boxWidth}`}>
-          Navbar
+          <Navbar></Navbar>
         </div>
       </div>
       
-      <div className={`bg-primary ${styles.flexStart}`}></div>
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero></Hero>
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats></Stats>
+          <Business></Business>
+          <Billing></Billing> 
+          <CardDeal></CardDeal> 
+          <Testimonials></Testimonials> 
+          <Client></Client> 
+          <CTA></CTA> 
+          <Footer></Footer> 
+
+        </div>
+      </div>
 
     </div>
   )
