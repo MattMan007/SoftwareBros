1e868452ef34d28f31a93c5a4d2a6adf2ef2ec62
MattMan007 <mateyonescu5@gmail.com>
2024-03-19 13:52:20 +0200

    card deal done

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { card } from '../assets'
+import styles, {layout} from '../style'
+import Button from './Button'
 
 const CardDeal = () => {
   return (
-    <div>CardDeal</div>
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>Find out how a website can <br className='sh:block hidden' /> greatly improove your business</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Having a modern style website can do wonders for your business. Atracting investors, young people you can hire and also bla bla bla. Not to mention the other advantages, bla bla bla...
+        </p>
+
+        <Button styles="mt-10"></Button>
+      </div>
+
+      <div className={layout.sectionImg}>
+        <img src={card} alt="card" className='w-[100%] h-[100%]' />
+      </div>
+
+    </section>
   )
 }
 
